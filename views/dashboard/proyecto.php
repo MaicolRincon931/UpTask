c4458a30c937978da50011fa91031560f2e66225
MaicolRincon931 <139707810+MaicolRincon931@users.noreply.github.com>
2024-04-15 19:35:43 -0500

    Lunes 15

---
+++ views/dashboard/proyecto.php
@@ -0,0 +1,26 @@
+<?php
+    include_once __DIR__ . '/header.php';
+?>
+
+<div class="contenedor-sm">
+<?php
+    include_once __DIR__ . '/../templates/alertas.php';
+?>
+
+
+<div class="contenedor-sm">
+    <div class="contenedor-nueva-tarea">
+        <button type="button" class="agregar-tarea" id="agregar-tarea">
+           &#43; Nueva Tarea
+        </button>
+    </div>
+</div>
+
+
+
+</div>
+
+
+<?php
+    include_once __DIR__ . '/footer.php';
+?>
